4bd3b74d80fa0d3c11aff9a2b76ca86c8e61ad51
Zhengguo Tan <zhengguo.tan@gmail.com>
2023-04-10 21:12:26 +0200

    original submission

---
+++ orig_submission/tar_all_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 
+# This script is used to tar LaTeX files and figures for revision
+
+tar -czvf LaTeX_all_files.tar.gz manuscripts/main.tex manuscripts/elsarticle.cls manuscripts/elsarticle-harv.cls manuscripts/ref.bib figures/fig1.png
